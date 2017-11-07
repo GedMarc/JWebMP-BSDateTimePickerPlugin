b2bd0e0975c9a1ab390229621a0313163d974b2b
Marc Magon <ged_marc@hotmail.com>
2017-11-07 02:43:26 +0200

    Coverage and Test Cases

--- src/za/co/mmagon/jwebswing/plugins/bs4datetimedropdown/BSDateTimeInputAttributes.java
+++ src/za/co/mmagon/jwebswing/plugins/bs4datetimedropdown/BSDateTimeInputAttributes.java
@@ -17,6 +17,7 @@
 package za.co.mmagon.jwebswing.plugins.bs4datetimedropdown;
 
 import za.co.mmagon.jwebswing.base.html.interfaces.AttributeDefinitions;
+import za.co.mmagon.jwebswing.utilities.StaticStrings;
 
 /**
  * @author GedMarc
@@ -34,7 +35,7 @@ public enum BSDateTimeInputAttributes implements AttributeDefinitions
 	@Override
 	public String toString()
 	{
-		return "data-" + super.toString().toLowerCase().replace('_', '-');
+		return "data-" + super.toString().toLowerCase().replace(StaticStrings.CHAR_UNDERSCORE, '-');
 	}
 	
 	@Override
